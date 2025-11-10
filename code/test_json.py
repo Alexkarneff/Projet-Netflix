b71859b8de7b3adadfd7bd3d2f622e1054853516
matthieucatteyfaye <matthieucatteyfaye@gmail.com>
2025-11-10 16:36:46 +0100

    Màj + suggestion utilisateur
    J'ai soumis une idée en commentaire dans le fichier test_json.py concernant le profil utilisateur.

--- code/test_json.py
+++ code/test_json.py
@@ -1,18 +1,24 @@
 import json
 
-donnees = {
-    "nom": "Stéphane ROBERT",
-    "age": 29,
-    "adresse": {
-        "rue": "456 avenue des Champs",
-        "ville": "Lyon",
-        "code_postal": "69001"
-    },
-    "hobbies": ["photographie", "voyage", "musique"]
-}
+infos_utilisateur = {
+    "nom": "à saisir ?",
+    "nb_connexion": 0, # à incrémenter de 1 à chaque lancement
+    "recherches_frequentes" : {[]} # liste ou dictionnaire regroupant les recherches 
+    }
+
+"""
+Idée : 
+Pour les recherches fréquentes; à chaque fois que l'utilisateur effectue une recherche faire : 
+if "recherche" in infos_utilisateur:
+    infos_utilisateur["recherche"] += 1
+else:
+    infos_utilisateur["recherche"] = 1
+"""
+
+
 
 # Conversion en chaîne JSON
-donnees_json = json.dumps(donnees, indent=4)
+# donnees_json = json.dumps(donnees, indent=4)
 
 # Affichage de la chaîne JSON
-print(donnees_json)
+# print(donnees_json)
