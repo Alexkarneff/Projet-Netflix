6f64883a7a545a78a632feebf0143fdcc3312bb4
matthieucatteyfaye <matthieucatteyfaye@gmail.com>
2025-11-13 14:20:05 +0100

    Màj code
    Misa à jour code : fonctions de recherche

--- code/projet_netflix.py
+++ code/projet_netflix.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-movies_dataset = pd.read_csv("data\movies_metadata_credits_joined.csv", sep = ",")
+movies_dataset = pd.read_csv("data\movies_metadata_credits_joined2.csv", sep = ",")
 
 # print(movies_dataset["original_title"].head())
 
@@ -13,52 +13,55 @@ movies_dataset = pd.read_csv("data\movies_metadata_credits_joined.csv", sep = ",
 
 # Fonction de recherche par durée : retourne un dataset contenant uniquement les films ayant exactement la bonne durée
 # Les lignes sont triées du film le plmus populaire au moins populaire
-def recherche_duree(dataset = movies_dataset):
-    while True:
-        try:
-            duree_saisie = float(input("Saisir une durée (en min.) : "))
-            new_dataset = dataset[dataset["runtime"] == duree_saisie]
-            new_dataset = new_dataset.sort_values("popularity", ascending=False)
-            break
-        except ValueError:
-            print("Saisie incorecte.")
+# def recherche_duree(dataset = movies_dataset):
+#     while True:
+#         try:
+#             duree_saisie = float(input("Saisir une durée (en min.) : "))
+#             new_dataset = dataset[dataset["runtime"] == duree_saisie]
+#             new_dataset = new_dataset.sort_values("popularity", ascending=False)
+#             break
+#         except ValueError:
+#             print("Saisie incorrecte.")
 
-    return new_dataset
+#     return new_dataset
 
-new_dataset = recherche_duree()
-print(new_dataset["original_title"].head())
+# new_dataset = recherche_duree()
+# print(new_dataset["original_title"].head())
 
 
 # Recherche par langue : fonctionne
-def recherche_langue(dataset = movies_dataset):
-    while True: 
+# def recherche_langue(dataset = movies_dataset):
+#     while True: 
+#         try:
+#             langue_saisie = input("Quelle langue ? ")
+#             langue_lettres = langue_saisie[0].lower()+langue_saisie[1].lower()
+#             new_dataset = dataset[dataset["original_language"]==langue_lettres]
+#             new_dataset = new_dataset.sort_values("popularity", ascending=False)
+#             break
+#         except IndexError:
+#             print("Saisie incorrecte.")
+
+#     return new_dataset
+
+# new_dataset = recherche_langue()
+# print(new_dataset["original_title"].head())
+
+
+
+def recherche_genre(dataset = movies_dataset):
+    while True:
         try:
-            langue_saisie = input("Quelle langue ? ")
-            langue_lettres = langue_saisie[0].lower()+langue_saisie[1].lower()
-            new_dataset = dataset[movies_dataset["original_language"]==langue_lettres]
+            genre_saisi = input("Saisir un genre : ")
+            genre_saisi = genre_saisi[0].upper() + genre_saisi[1:].lower()
+            new_dataset = dataset[dataset['genres_names'].str.contains(genre_saisi, na=False)]
             new_dataset = new_dataset.sort_values("popularity", ascending=False)
             break
-        except IndexError:
-            print("Saisie incorecte.")
+        except ValueError:
+            print("Saisie incorrecte.")
 
     return new_dataset
 
-new_dataset = recherche_langue()
+new_dataset = recherche_genre()
 print(new_dataset["original_title"].head())
-
-
-# def recherche_genre(genre_saisi, dataset = movies_dataset):
-#     films_filtres = []
-#     for i in dataset["genres"]:
-#         for j in i:
-#             if j[1] == genre_saisi:
-#                 films_filtres.append(dataset[dataset["genres"]==i]["original_title"])
-    
-#     return films_filtres
-
-
-# new_dataset = recherche_genre('Family')
-# print(new_dataset)
-
 
 
