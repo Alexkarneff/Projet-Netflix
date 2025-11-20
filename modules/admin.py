140e46d931d883374a41298bcebf09c358bd9655
Alexkarneff <alexkarneff@gmail.com>
2025-11-20 18:04:46 +0100

    admin

--- modules/admin.py
+++ modules/admin.py
@@ -6,11 +6,10 @@ USER_FILE = "data/users.json"
 def affichage_admin():
         # Affiche le menu principaladmin.
     print("\n=== MENU ADMIN ===")
-    print("1. Supprimer les données de tous les utilisateurs")
-    print("2. Afficher le nombre d'utilisateurs inscrits")
+    print("1. Afficher le nombre d'utilisateurs inscrits")
+    print("2. Supprimer les données de tous les utilisateurs")
+
     print("q. Quitter le menu Admin")
-
-
 
 
 def programme_admin():
@@ -21,13 +20,14 @@ def programme_admin():
         choix = input("Choisissez une option : ").strip()
         match choix :
             case "1":
+                users = utilisateur.load_users()
+                print(f"Nombre d'utilisateurs différents : {len(users)}")
+            case "2":
                 confirmation = input("Êtes-vous sûr de vouloir supprimer TOUTES les données ? (oui/non) : ")
                 if confirmation.lower() == "oui":
                     utilisateur.save_users({})  # sauvegarde un dictionnaire vide
                     print("Toutes les données ont été supprimées")
-            case "2":
-                users = utilisateur.load_users()
-                print(f"Nombre d'utilisateurs différents : {len(users)}")
 
-            case "q" | "quit" | "quitter":
+
+            case "q" | "quit" | "quitter":              # quitter le menu admin
                 programme_admin= False
